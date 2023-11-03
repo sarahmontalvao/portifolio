4729a556b950fa83ccb4b0992ec83da4db5de664
Sarah <sarahmonttalvao@gmail.com>
2023-11-03 20:25:25 -0300

    Add projeto e descrição

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-function isElementInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-
-const target = document.querySelectorAll('[data-anime]');
-
-const animationClass = 'animate';
-
-function animeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element){
-        if((windowTop) > element.offsetTop){
-            element.classList.add(animationClass);
-        }else{
-            element.classList.remove(animationClass);
-        }
-    })
-}
-
-animeScroll();
-
-if(target.length){
-    window.addEventListener('scroll', function (){
-        animeScroll();
-    })
-}
-
-//projetos
-
 const projetos = [
+
+    {
+        titulo: 'CineSala',
+        img:'img/cinema.png',
+        code: 'https://github.com/sarahmontalvao/cinema/tree/main/src',
+        site:'https://cinema-4gqmy1g17-sarahs-projects-4a16e032.vercel.app/',
+        tecno: 'React, Typescript',
+        desc: 'O CineSala é um projeto desenvolvido com React, TypeScript, HTML e CSS, oferecendo aos usuários uma experiência imersiva e fácil de usar para explorar, buscar e favoritar filmes. Sua interface é fluida e responsiva, garantindo uma navegação suave em todas as plataformas.',
+    
+    },
     {
     titulo: 'Jogo da Cobrinha',
     img:'img/cobrinha.png',
     code: 'https://github.com/sarahmontalvao/jogoDaCobrinha/blob/main/js/snake.js',
     site:'https://sarahmontalvao.github.io/jogoDaCobrinha',
-
+    tecno: 'Html, Css, Javascript',
+    desc: 'O Jogo da Cobrinha em JavaScript é um clássico dos videogames, onde os jogadores controlam uma cobra usando as setas do teclado para se movimentar pelo campo de jogo. A cobra é representada como um array de segmentos coloridos, que aumentam de tamanho ao comer alimentos, também representados por blocos coloridos.',
 },
 
 {
@@ -48,7 +23,8 @@ const projetos = [
     img:'img/lista.png',
     code: 'https://github.com/sarahmontalvao/lista-de-tarefas/blob/main/js/controle.js',
     site:'https://sarahmontalvao.github.io/lista-de-tarefas/',
-
+    tecno: 'Html, Css, Javascript',
+    desc: 'A lista de tarefas em JavaScript é uma aplicação simples que permite aos usuários adicionar, marcar como concluídas ou excluir tarefas. Utilizando HTML, CSS e JavaScript, as tarefas são exibidas como itens na página. Os usuários podem adicionar novas tarefas, marcá-las como concluídas com um clique e remover tarefas indesejadas.',
 },
 
 {
@@ -56,6 +32,8 @@ const projetos = [
     img:'img/mosquito.png',
     code: 'https://github.com/sarahmontalvao/mata-mosquito-game/blob/main/app.js',
     site:'https://sarahmontalvao.github.io/mata-mosquito-game/',
+    tecno: 'Html, Css, Javascript',
+    desc: 'Neste jogo, mosquitos aparecem aleatoriamente na tela, e os jogadores têm que clicar neles para eliminá-los. Usei setInterval() para definir se o usuario ganhou ou perdeu, proporcionando uma experiência de jogo dinâmica. Além disso, implementei um sistema de seleção de níveis, permitindo aos jogadores escolherem entre diferentes graus de dificuldade para um desafio personalizado.',
 
 },
 
@@ -64,7 +42,8 @@ const projetos = [
     img:'img/relogio.png',
     code: 'https://github.com/sarahmontalvao/relogio/blob/main/js/script.js',
     site:'https://sarahmontalvao.github.io/relogio/',
-
+    tecno: 'Html, Css, Javascript',
+    desc: 'Relógio digital e analógico em JavaScript. O relógio digital exibe a hora de forma clara, enquanto o relógio analógico apresenta ponteiros elegantes. Ambos atualizam em tempo real.',
 },
 
 
@@ -88,31 +67,57 @@ projetos.forEach((projeto) => {
     site.classList.add('button');
     site.innerHTML = 'site'
     site.href = projeto.site;
+    site.target = '_blank';
     
     const code = document.createElement('a');
     code.classList.add('button');
     code.innerHTML = 'codigo'
     code.href = projeto.code;
+    code.target = '_blank';
+
+    const tecno = document.createElement('p');
+    tecno.innerHTML = projeto.tecno
+    tecno.classList.add('tecnoProjeto')
    
 
     const titulo = document.createElement('p');
     titulo.innerHTML = projeto.titulo;
+    titulo.classList.add('tituloProjeto')
 
     const imgDiv = document.createElement('div');
     imgDiv.classList.add('imgProject');
 
     const img = document.createElement('img')
     img.src = projeto.img;
+
+    const desc = document.createElement('div')
+    desc.innerHTML = projeto.desc;
+    desc.classList.add('desc');
+    desc.style.display = 'none';
+
+    imgDiv.appendChild(desc); 
+
+       
+    imgDiv.addEventListener('mouseover', () => {
+      img.style.display = 'none';
+      desc.style.display = 'flex';
+  });
+
+ 
+  imgDiv.addEventListener('mouseout', () => {
+    img.style.display = 'block';
+      desc.style.display = 'none';
+  });
+
     
    
     projectDiv.appendChild(button);
+    button.appendChild(titulo)
     button.appendChild(site);
     button.appendChild(code);
+    button.appendChild(tecno);
     projectDiv.appendChild(imgDiv);
     imgDiv.appendChild(img)
-    
-    
-    
 
     container.appendChild(projectDiv);
 });
@@ -128,7 +133,6 @@ hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
 
-// Adicione um evento de clique a cada link
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.remove("active"); 
